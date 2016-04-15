2d089da97036239e74a8834334b8e306ba75c416
Tankobot <tankobot@users.noreply.github.com>
2016-04-15 16:29:15 -0700

    Create read method (not finished).

--- core/Reader.py
+++ core/Reader.py
@@ -2,4 +2,15 @@ class Reader:
     """File-like object for text analysis."""
 
     def __init__(self, source):
-        assert source.read
+        self.source = source
+        self.last = None
+        self.char = ''
+
+    def read(self, n=1):
+        pulled = 0
+        while pulled < n:
+            self.char = self.char or self.source.read(1)
+
+            # TODO 
+
+            self.char = ''
